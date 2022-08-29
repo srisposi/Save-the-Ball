f9927c35941a818e6b1d70af7a25e7e610db9c8c
srisposi <sdrisposi@gmail.com>
2022-08-29 17:05:29 -0300

    Create the Enemy

--- main.lua
+++ main.lua
@@ -1,10 +1,14 @@
-_G.love = require("love")
+local love = require "love"
+local enemy = require "Enemy"
+
+math.randomseed(os.time())
 
 local game = {
+    difficulty = 1,
     state = {
-        menu = true,
+        menu = false,
         paused = false,
-        running = false,
+        running = true,
         ended = false,
     }
 }
@@ -15,19 +19,31 @@ local player = {
     y = 30,
 }
 
+local enemies = {}
+
 function love.load()
     love.window.setTitle("Save the Ball")
     love.mouse.setVisible(false)
+
+    table.insert(enemies, 1, enemy())
 end
 
 function love.update(dt)
     player.x, player.y = love.mouse.getPosition()    
+
+    for i = 1, #enemies do
+        enemies[i]:move(player.x, player.y)
+    end
 end
 
 function love.draw()
     love.graphics.printf("FPS: " .. love.timer.getFPS(), love.graphics.newFont(16), 10, love.graphics.getHeight() -30, love.graphics.getWidth())
 
     if game.state["running"] then
+        for i = 1, #enemies do
+            enemies[i]:drwa()
+        end 
+
         love.graphics.circle("fill", player.x, player.y, player.radius)
     end
 
